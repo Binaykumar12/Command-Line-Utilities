090d9fd09fd21abde119a0fb50bbdc34477640a1
Binaykumar12 <binaykumarkurmi78@gmail.com>
2024-07-24 21:05:49 +0545

    .

--- my_module.py
+++ my_module.py
@@ -1,12 +1,17 @@
 # my_module.py
 import requests
+from bs4 import BeautifulSoup
 
-def scrape_website(url):
-    """Function to scrape data from a website"""
+def scrape_books(url):
+    """Function to scrape book titles from a website"""
     response = requests.get(url)
-    return response.text
+    soup = BeautifulSoup(response.text, 'html.parser')
+    books = soup.find_all('h3')
+    book_titles = [book.get_text() for book in books]
+    return book_titles
 
 def save_to_file(data, filename):
     """Function to save scraped data to a file"""
     with open(filename, 'w') as file:
-        file.write(data)
+        for item in data:
+            file.write(f"{item}\n")
